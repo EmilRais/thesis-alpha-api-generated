2660fc2be3390ee6f8944c524a56a9fd170b4f00
Emil Rais <emilrais@outlook.com>
2017-11-04 11:38:07 +0100

    Updated validation to accept schema

--- validation/source/main.ts
+++ validation/source/main.ts
@@ -1,13 +1,27 @@
 import { RequestHandler } from "express";
+import { Rule } from "paradise";
 
 import { BoardRule } from "./board.rule";
+import { UserRule } from "./user.rule";
 
 export interface AbstractOperation {
     module: string;
+    schema: Schema;
+}
+
+type Schema = "board" | "user";
+
+function selectStrategy(schema: Schema): Rule {
+    switch ( schema ) {
+        case "board": return BoardRule();
+        case "user": return UserRule();
+        default: throw new Error(`validation could not recognise schema "${schema}"`);
+    }
 }
 
 export const prepareOperation = (abstractOperation: AbstractOperation) => {
-    const rule = BoardRule();
+    if ( !abstractOperation.schema ) return Promise.reject("validation expected a schema");
+    const rule = selectStrategy(abstractOperation.schema);
 
     const concreteOperation: RequestHandler = (request, response, next) => {
         rule.guard(request.body)
--- validation/test/main.test.ts
+++ validation/test/main.test.ts
@@ -11,9 +11,9 @@ import { AbstractOperation, prepareOperation } from "../source/main";
 
 describe("operation", () => {
 
-    it("should reject with 400 if input is invalid", () => {
+    it("should reject with 400 if input is invalid board", () => {
         const board = { name: "some-name", image: 42 };
-        const abstractOperation: AbstractOperation = { module: "validation" };
+        const abstractOperation: AbstractOperation = { module: "validation", schema: "board" };
         return prepareOperation(abstractOperation)
             .then(operation => {
                 return new Promise((resolve, reject) => {
@@ -37,9 +37,9 @@ describe("operation", () => {
             });
     });
 
-    it("should continue to next operation if input is valid", () => {
+    it("should continue to next operation if input is valid board", () => {
         const board = { name: "some-name", image: "some-image" };
-        const abstractOperation: AbstractOperation = { module: "validation" };
+        const abstractOperation: AbstractOperation = { module: "validation", schema: "board" };
         return prepareOperation(abstractOperation)
             .then(operation => {
                 return new Promise((resolve, reject) => {
@@ -51,6 +51,64 @@ describe("operation", () => {
                             const runningServer: Server = this;
                             agent.post("localhost:3030")
                                 .send(board)
+                                .catch(error => error.response)
+                                .then(response => {
+                                    runningServer.close();
+                                    response.status.should.equal(200);
+                                    resolve();
+                                })
+                                .catch(reject);
+                        });
+                });
+            });
+    });
+
+    it("should reject with 400 if input is invalid user", () => {
+        const user = {
+            email: "some-invalid-email",
+            credential: { type: "alpha-api", email: "some@email.dk", password: "some-password" }
+        };
+        const abstractOperation: AbstractOperation = { module: "validation", schema: "user" };
+        return prepareOperation(abstractOperation)
+            .then(operation => {
+                return new Promise((resolve, reject) => {
+                    express()
+                        .use(json())
+                        .use(operation)
+                        .listen(3030, function() {
+                            const runningServer: Server = this;
+                            agent.post("localhost:3030")
+                                .send(user)
+                                .catch(error => error.response)
+                                .then(response => {
+                                    runningServer.close();
+                                    response.status.should.equal(400);
+                                    response.body.should.deep.equal(['"$.email" was not an email address']);
+                                    resolve();
+                                })
+                                .catch(reject);
+                        });
+                });
+            });
+    });
+
+    it("should continue to next operation if input is valid user", () => {
+        const user = {
+            email: "some@email.dk",
+            credential: { type: "alpha-api", email: "some@email.dk", password: "some-password" }
+        };
+        const abstractOperation: AbstractOperation = { module: "validation", schema: "user" };
+        return prepareOperation(abstractOperation)
+            .then(operation => {
+                return new Promise((resolve, reject) => {
+                    express()
+                        .use(json())
+                        .use(operation)
+                        .use(success)
+                        .listen(3030, function() {
+                            const runningServer: Server = this;
+                            agent.post("localhost:3030")
+                                .send(user)
                                 .catch(error => error.response)
                                 .then(response => {
                                     runningServer.close();
