90a24f0721685931ffdcf5d56da6894faea317e1
Emil Rais <emilrais@outlook.com>
2017-11-24 11:52:47 +0100

    mongo-update can now be configured with the query

--- mongo-update/source/main.ts
+++ mongo-update/source/main.ts
@@ -1,15 +1,31 @@
 import { RequestHandler } from "express";
+import * as fs from "fs";
 import { MongoClient, ObjectId } from "mongodb";
+import { resolve as resolvePath } from "path";
+
+import { Evaluator } from "./evaluator";
+const evaluator = new Evaluator();
 
 export interface AbstractOperation {
     module: string;
     collection: string;
+    query: string;
     host?: string;
 }
 
-export const prepareOperation = (abstractOperation: AbstractOperation) => {
+interface Query {
+    selector: any;
+    update: any;
+}
+
+export const prepareOperation = (abstractOperation: AbstractOperation, context: string) => {
     const collection = abstractOperation.collection;
     if ( !collection ) return Promise.reject("mongo-update expected a collection");
+
+    const query = abstractOperation.query;
+    if ( !query ) return Promise.reject("mongo-update expected a query");
+    const queryPath = resolvePath(context, query);
+    const queryObject = JSON.parse(fs.readFileSync(queryPath).toString());
 
     const host = abstractOperation.host || "mongo";
 
@@ -17,17 +33,10 @@ export const prepareOperation = (abstractOperation: AbstractOperation) => {
         .then(database => {
 
             const concreteOperation: RequestHandler = (request, response, next) => {
-                const user = response.locals.boards;
-                const filter = { _id: user._id };
-
-                const credential = request.body;
-                const update = {
-                    $set: { credential: credential }
-                };
-
+                const actualQuery: Query = evaluator.evaluate(request, response, queryObject);
                 const options = { returnOriginal: false };
 
-                database.collection(collection).findOneAndUpdate(filter, update, options)
+                database.collection(collection).findOneAndUpdate(actualQuery.selector, actualQuery.update, options)
                     .then(result => {
                         response.locals.boards = result.value;
                         next();
--- mongo-update/test/main.test.ts
+++ mongo-update/test/main.test.ts
@@ -25,32 +25,36 @@ describe("operation", () => {
     });
 
     it("should fail if collection has not been specified", () => {
-        const abstractOperation: AbstractOperation = { module: "mongo-update", collection: null, host: "localhost" };
-        return prepareOperation(abstractOperation)
+        const abstractOperation: AbstractOperation = { module: "mongo-update", collection: null, query: null };
+        return prepareOperation(abstractOperation, null)
             .then(() => Promise.reject("Expected failure"))
             .catch(error => error.should.equal("mongo-update expected a collection"));
     });
 
-    it("should update user's credential", () => {
-        const abstractOperation: AbstractOperation = { module: "mongo-update", collection: "Users", host: "localhost" };
-        return prepareOperation(abstractOperation)
+    it("should fail if query has not been specified", () => {
+        const abstractOperation: AbstractOperation = { module: "mongo-update", collection: "some-collection", query: null };
+        return prepareOperation(abstractOperation, null)
+            .then(() => Promise.reject("Expected failure"))
+            .catch(error => error.should.equal("mongo-update expected a query"));
+    });
+
+    it("should perform update", () => {
+        const abstractOperation: AbstractOperation = { module: "mongo-update", collection: "Users", query: "queries/simple-query.json", host: "localhost" };
+        return prepareOperation(abstractOperation, "test")
             .then(operation => {
                 return new Promise((resolve, reject) => {
                     express()
                         .use(json())
-                        .use(updateBoards({ _id: "some-id" }))
                         .use(operation)
                         .listen(3030, function() {
                             const runningServer = this;
 
                             const user = {
                                 _id: "some-id",
-                                name: "some-name",
-                                credential: null as any
+                                age: 1
                             };
-                            const credential = { userId: "some-user-id", token: "some-token" };
                             database.collection("Users").insert(user)
-                                .then(() => agent.post("localhost:3030").send(credential).catch(error => error.response))
+                                .then(() => agent.post("localhost:3030").catch(error => error.response))
                                 .then(response => {
                                     (operation as any).database.close();
                                     runningServer.close();
@@ -59,8 +63,7 @@ describe("operation", () => {
                                         .then((boards: any) => {
                                             boards.should.deep.equal([{
                                                 _id: "some-id",
-                                                name: "some-name",
-                                                credential: credential
+                                                age: 2
                                             }]);
                                             resolve();
                                         });
@@ -72,13 +75,12 @@ describe("operation", () => {
     });
 
     it("should store updated object in response.locals.boards", () => {
-        const abstractOperation: AbstractOperation = { module: "mongo-update", collection: "Users", host: "localhost" };
-        return prepareOperation(abstractOperation)
+        const abstractOperation: AbstractOperation = { module: "mongo-update", collection: "Users", query: "queries/simple-query.json", host: "localhost" };
+        return prepareOperation(abstractOperation, "test")
             .then(operation => {
                 return new Promise((resolve, reject) => {
                     express()
                         .use(json())
-                        .use(updateBoards({ _id: "some-id" }))
                         .use(operation)
                         .use(responseHandler)
                         .listen(3030, function() {
@@ -86,42 +88,24 @@ describe("operation", () => {
 
                             const user = {
                                 _id: "some-id",
-                                name: "some-name",
-                                credential: null as any
+                                age: 1
                             };
-                            const credential = { userId: "some-user-id", token: "some-token" };
                             database.collection("Users").insert(user)
-                                .then(() => agent.post("localhost:3030").send(credential).catch(error => error.response))
+                                .then(() => agent.post("localhost:3030").catch(error => error.response))
                                 .then(response => {
                                     (operation as any).database.close();
                                     runningServer.close();
 
-                                    response.status.should.equal(200);
-                                    response.body.should.deep.equal({
-                                        _id: "some-id",
-                                        name: "some-name",
-                                        credential: credential
-                                    });
+                                    response.body.should.deep.equal({ _id: "some-id", age: 2 });
                                     resolve();
                                 })
                                 .catch(reject);
                         });
                 });
             });
-    });
+        });
 });
-
-const updateBoards: (boards: any) => RequestHandler = boards => {
-    return (request, response, next) => {
-        response.locals.boards = boards;
-        next();
-    };
-};
 
 const responseHandler: RequestHandler = (request, response, next) => {
     response.status(200).json(response.locals.boards);
-};
-
-const errorHandler: ErrorRequestHandler = (error, request, response, next) => {
-    response.status(500).end(error.message);
 };
